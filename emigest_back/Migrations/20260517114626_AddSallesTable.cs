eae63e11fbcce9243d0dbc9d6fb93905b1fb346e
nyainafaneva <nyainafaneva2006@gmail.com>
2026-05-19 21:05:58 +0300

    Premier maj backend

---
+++ emigest_back/Migrations/20260517114626_AddSallesTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace emigest_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSallesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
